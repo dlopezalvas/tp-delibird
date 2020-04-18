6bb02936e7054a5430dfcc318e5debb7b704388b
Gaston Pulleiro <gpulleiro.utn@gmail.com>
2020-04-18 20:37:05 -0300

    Configuracion y log de GameBoy

--- GameBoy/src/GameBoy.c
+++ GameBoy/src/GameBoy.c
@@ -8,10 +8,61 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "GameBoy.h"
+
+
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+
+//	 char * linea;
+//	  while(1) {
+//	    linea = readline(">");
+//	    if(linea)
+//	      add_history(linea);
+//
+//	    if("BROKER" == linea){
+//
+//	    	puts("BROKER");
+//
+//	    }
+//
+//	    if(!strncmp(linea, "exit", 4)) {
+//	       free(linea);
+//	       break;
+//	    }
+//	    printf("%s\n", linea);
+//	    free(linea);
+//	  }
+
+
+	  char* ip;
+	  	char* puerto;
+
+	  	t_log* logger;
+	  	t_config* config;
+
+	  	logger = iniciar_logger();
+
+	  	log_info(logger,"-----------LOG START--------");
+	  	//Loggear "soy un log"
+
+	  	config = leer_config();
+
+
+	  	/*---------------------------------------------------PARTE 3-------------------------------------------------------------*/
+
+	  	//antes de continuar, tenemos que asegurarnos que el servidor esté corriendo porque lo necesitaremos para lo que sigue.
+	  	ip = config_get_string_value(config,"IP");
+	  	puerto = config_get_string_value(config,"PUERTO");
+	  	log_info(logger,puerto);
+	  	log_info(logger,ip);
+
+	  log_destroy(logger);
+	  config_destroy(config);
+
 }
+
+
+
+
+
---
+++ GameBoy/src/GameBoy.h
@@ -0,0 +1,20 @@
+/*
+ * GameBoy.h
+ *
+ *  Created on: 18 abr. 2020
+ *      Author: MCLDG
+ */
+
+#ifndef GAMEBOY_H_
+#define GAMEBOY_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<readline/readline.h>
+
+#include "utils.h"
+
+
+
+
+#endif /* GAMEBOY_H_ */
