f5405facac669ac499365eb6c26464b3721eadb3
lucas francisco lopez <lopezlucasf@gmail.com>
2020-05-06 19:48:32 -0300

    Add common-functions for Broker

--- Broker/src/utils_broker.c
+++ Broker/src/utils_broker.c
@@ -2,3 +2,33 @@
 #include "utils_broker.h"
 
 
+void recibir_mensaje_queue(t_mensaje* mensaje){
+	if(proceso_valido(mensaje -> proceso))
+		printf("%s","El proceso no esta autorizado para suscribirse o mandar mensajes");
+
+	if(queue_valido(mensaje -> queue))
+		printf("%s","La queue no existe");
+
+}
+
+int proceso_valido(char* proceso){
+
+	//tener en el config un array de procesos
+	//fijarse si proceso se encuentra en ese array y devolver true
+
+	return 0;
+}
+
+int queue_valido(char* queue){
+
+	//tener en el config un array de procesos
+	//fijarse si proceso se encuentra en ese array y devolver true
+
+	return 0;
+}
+
+void suscribir_mensaje_queue(t_mensaje* mensaje){
+	//loggear que el proceso <mensaje -> proceso> se suscribio a la cola <mensaje -> queue>
+
+	//pushear mensaje a la cola correspondiente
+}
--- Broker/src/utils_broker.h
+++ Broker/src/utils_broker.h
@@ -14,6 +14,19 @@
 #include<../CommonsMCLDG/utils.h>
 
 
+typedef struct{
+  char *proceso;
+  char **parametros; //ejemplo: ["PARAM1","PARAM2","PARAM3"]
+  char *queue;
+}  t_mensaje;
+
+//TODO: Recibe un mensaje desde un suscriptor y lo deserializa transofrmando a un t_mensaje
+void recibir_mensaje_broker(void);
+
+void recibir_mensaje_queue(t_mensaje*);
+int proceso_valido(char*);
+int queue_valido(char*);
+void suscribir_mensaje_queue(t_mensaje*);
 
 
 #endif /* UTILS_BROKER_H_ */
