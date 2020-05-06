7bc2e35bf747201fa51b819d27e03fa717f678ed
lucas francisco lopez <lopezlucasf@gmail.com>
2020-05-06 20:05:38 -0300

    Interfaces funciones checkpoint1 broker

--- Broker/src/utils_broker.c
+++ Broker/src/utils_broker.c
@@ -1,14 +1,20 @@
 
 #include "utils_broker.h"
 
+void recibir_mensaje_broker(void){
+	//recibe un mensaje, tiene que deserializarlo y llamar a recibir_mensaje_queue
+	//generar el id unico (Ver el tipo, quiza tenga que ser un uuid)
+	//pasandole el tipo t_mensaje*
+};
 
 void recibir_mensaje_queue(t_mensaje* mensaje){
 	if(proceso_valido(mensaje -> proceso))
 		printf("%s","El proceso no esta autorizado para suscribirse o mandar mensajes");
 
-	if(queue_valido(mensaje -> queue))
+	if(queue_valida(mensaje -> queue))
 		printf("%s","La queue no existe");
 
+	//suscribir_mensaje_queue
 }
 
 int proceso_valido(char* proceso){
@@ -19,7 +25,7 @@ int proceso_valido(char* proceso){
 	return 0;
 }
 
-int queue_valido(char* queue){
+int queue_valida(char* queue){
 
 	//tener en el config un array de procesos
 	//fijarse si proceso se encuentra en ese array y devolver true
@@ -28,7 +34,15 @@ int queue_valido(char* queue){
 }
 
 void suscribir_mensaje_queue(t_mensaje* mensaje){
-	//loggear que el proceso <mensaje -> proceso> se suscribio a la cola <mensaje -> queue>
+	//loggear que el proceso <mensaje -> proceso> se suscribia a la cola <mensaje -> queue>
 
 	//pushear mensaje a la cola correspondiente
+}
+
+void crear_queues(void){
+	//Crea las colas de las queues traidas del config
+}
+
+void terminar_queues(void){
+	//libera de la memoria todas las queues
 }
--- Broker/src/utils_broker.h
+++ Broker/src/utils_broker.h
@@ -18,15 +18,29 @@ typedef struct{
   char *proceso;
   char **parametros; //ejemplo: ["PARAM1","PARAM2","PARAM3"]
   char *queue;
+  uint32_t *id;
 }  t_mensaje;
 
-//TODO: Recibe un mensaje desde un suscriptor y lo deserializa transofrmando a un t_mensaje
+//Recibe un mensaje desde un suscriptor y lo deserializa transofrmando a un t_mensaje
 void recibir_mensaje_broker(void);
 
+//
 void recibir_mensaje_queue(t_mensaje*);
+
+//Valida que un proceso sea existente
 int proceso_valido(char*);
-int queue_valido(char*);
+
+//Valida que una queue sea valida
+int queue_valida(char*);
+
+//Suscribe un mensaje de tipo t_mensaje a una queue
 void suscribir_mensaje_queue(t_mensaje*);
+
+//Inicializa todas las queues
+void crear_queues();
+
+//Libera la memoria de las queues
+void terminar_queues();
 
 
 #endif /* UTILS_BROKER_H_ */
