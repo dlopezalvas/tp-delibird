c583e9a8ce0ba33f2294a2a3ad16f04a463c951e
Diana Lopez Alvas <dlopezalvas@gmail.com>
2020-06-11 17:20:24 -0300

    bitmap creado

--- GameCard/src/utils_gamecard.c
+++ GameCard/src/utils_gamecard.c
@@ -4,18 +4,21 @@
 //creacion de archivos y directorios (checkpoint 3)
 
 void crear_tall_grass(t_config* config){
-	char* punto_de_montaje = config_get_string_value(config,PUNTO_MONTAJE_TALLGRASS);
+	pto_montaje = config_get_string_value(config,PUNTO_MONTAJE_TALLGRASS);
 
-	mkdir(punto_de_montaje, 0777);
+	mkdir(pto_montaje, 0777);
 
-	crear_metadata(config);
+	crear_metadata(pto_montaje);
+	crear_bitmap(pto_montaje);
 }
 
-void crear_metadata(t_config* config){
+void crear_metadata(char* punto_montaje){
 
-	char* path_metadata = config_get_string_value(config,PUNTO_MONTAJE_TALLGRASS);
+	char* path_metadata = string_new();
 
+	string_append(&path_metadata, punto_montaje);
 	string_append(&path_metadata,"/Metadata");
+
 	mkdir(path_metadata, 0777);
 
 	string_append(&path_metadata, "/Metadata.bin");
@@ -32,6 +35,23 @@ void crear_metadata(t_config* config){
 
 }
 
+void crear_bitmap(char* punto_montaje){
+
+	char* path_bitarray = string_new();
+
+	string_append(&path_bitarray, punto_montaje);
+	string_append(&path_bitarray,"/Metadata/Bitmap.bin");
+
+	int blocks = 5192/8; //esto esta para probar, depues hay tirar datos del metadata a alguna estructura
+
+	int bitarray_file = open(path_bitarray, O_RDWR | O_CREAT);  //uso open porque necesito el int para el mmap
+
+	char* mapeo_bitarray = mmap(0, blocks, PROT_WRITE | PROT_READ, MAP_SHARED, bitarray_file, 0);
+
+	bitarray = bitarray_create(mapeo_bitarray, blocks); //MODE DEFAULT
+
+	free(path_bitarray);
+}
 
 
 
--- GameCard/src/utils_gamecard.h
+++ GameCard/src/utils_gamecard.h
@@ -11,16 +11,22 @@
 #include<commons/log.h>
 #include<commons/string.h>
 #include<commons/config.h>
+#include<commons/bitarray.h>
 #include<../CommonsMCLDG/utils.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/mman.h>
 
 #define PUNTO_MONTAJE_TALLGRASS "PUNTO_MONTAJE_TALLGRASS"
 
+t_bitarray* bitarray;
+char* pto_montaje;
+
 
 void crear_tall_grass(t_config* config);
-void crear_metadata(t_config* config);
+void crear_metadata(char* path_metadata);
+void crear_bitmap(char* path);
 
 
 
