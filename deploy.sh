08850bdc10ca173af7ca95bc1cf324b319c80d36
Diana Lopez Alvas <dlopezalvas@gmail.com>
2020-08-06 20:54:50 -0300

    deploy con workspace

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@ echo "Deploy TP"
 cd so-commons-library
 sudo make install
 cd ~
-cd tp-2020-1c-MCLDG/CommonsMCLDG/Debug
+cd workspace/tp-2020-1c-MCLDG/CommonsMCLDG/Debug
 make clean
 make all
 echo "Por Instalar el LD_LIBRARY_PATH, el actual valor es $LD_LIBRARY_PATH"
-export LD_LIBRARY_PATH="/home/utnso/tp-2020-1c-MCLDG/CommonsMCLDG/Debug"
+export LD_LIBRARY_PATH="/home/utnso/workspace/tp-2020-1c-MCLDG/CommonsMCLDG/Debug"
 echo "Cambiado a $LD_LIBRARY_PATH"
 cd ..
 cd ..
